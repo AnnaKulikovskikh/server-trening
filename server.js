c739a7ce5b97ce9fe738c6f86e9c877572659718
Anna <k_anna_i@mail.ru>
2022-04-19 23:11:39 +0300

    name phone

--- server.js
+++ server.js
@@ -1,100 +1,40 @@
+//экспорт
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+
 const app = new Koa();
 
 
+//пример сохраняемого объекта
 const messages = [
-    {id: 1,
-     text: "подключить телефон к.201",
-     created: "220222 9:10"
+    {
+     name: "JOe",
+     phone: "+7-125-456-78-45"
     },
 ];
-
-let newId = 2;
 
 app.use(koaBody({
     urlencoded: true,
     multipart: true,
   }));
 
-  app.use(async (ctx, next) => {
-    const origin = ctx.request.get('Origin');
-    if (!origin) {
-      return await next();
-    }
-    const headers = { 'Access-Control-Allow-Origin': '*', };
-  
-    if (ctx.request.method !== 'OPTIONS') {
-      ctx.response.set({...headers});
-      try {
-        return await next();
-      } catch (e) {
-        e.headers = {...e.headers, ...headers};
-        throw e;
-      }
-    }
-  
-    if (ctx.request.get('Access-Control-Request-Method')) {
-      ctx.response.set({
-        ...headers,
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-      });
-  
-      if (ctx.request.get('Access-Control-Request-Headers')) {
-        ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-      }
-      ctx.response.status = 204;
-    }
-  });
-
 app.use(async ctx => {
-    ctx.response.body ='server response';
-
-    const method = ctx.request.query.method;
-
-    switch (method) {
-        case 'allMessages':
-            ctx.response.body = messages;
-            return;
-        case 'messageById':
-          const id = Number(ctx.request.query.id);
-          const message = messages.find(elem => id === elem.id);
-          ctx.response.body = message;
-          return;
-        case 'createMessage':
-           if (ctx.request.query.id) {
-             //const { name, description, time, status } = ctx.request.body;
-             const { text, time, status } = ctx.request.body;
-             const editId = Number(ctx.request.query.id);
-             messages.forEach((elem) => {
-               if (editId === elem.id) {
-                 if (!text) {
-                   elem.status = status;
-                 } else {
-                  elem.test = test;
-                  elem.time = time;
-                  elem.status = status;
-                 }  
-               }
-             });
-           } else {
-             messages.push(Object.assign({id: newId}, ctx.request.body));
-             newId += 1;
-           }
-           ctx.response.body = true;
-           return;
-        case 'delMessageById':
-          const delid = Number(ctx.request.query.id);
-          const index = messages.findIndex(elem => delid === elem.id);
-          messages.splice(index, 1);
-          ctx.response.body = true;
-          return;
-        default:
-            ctx.response.status = 404;
-            return;
-    }
-});
+  const {name, phone} = ctx.request.querystring;
+  console.log(ctx.request.querystring);
+  console.log(ctx.request.body);
+  ctx.response.body = 'server response';
+  ctx.response.set({
+    'Access-Control-Allow-Origin': '*'
+  });
+  if (subscriptions.has(phone)) {
+    ctx.response.status = 400
+    ctx.response.body = 'You already subscribed'
+    return;
+  }
+  subscriptions.set(phone, name);
+  ctx.response.body = 'Ok';
+})
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback()).listen(port);
